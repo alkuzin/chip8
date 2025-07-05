0508e8e07e2fd4585656aa21dfac53ad60b95a97
alkuzin <alkuzindev@gmail.com>
2025-07-05 15:56:12 +0300

    [feat]: added title() & help() methods for Config

--- src/config.rs
+++ src/config.rs
@@ -10,7 +10,7 @@ impl Config {
     /// Get software version.
     ///
     /// # Returns
-    /// String representation of software version.
+    /// - String representation of software version.
     pub const fn version() -> &'static str {
         concat!(
             "v",
@@ -25,7 +25,7 @@ impl Config {
     /// Get software name.
     ///
     /// # Returns
-    /// String representation of software name.
+    /// - String representation of software name.
     pub const fn name() -> &'static str {
         env!("CARGO_PKG_NAME")
     }
@@ -33,7 +33,7 @@ impl Config {
     /// Get software description.
     ///
     /// # Returns
-    /// String representation of software description.
+    /// - String representation of software description.
     pub const fn description() -> &'static str {
         env!("CARGO_PKG_DESCRIPTION")
     }
@@ -41,7 +41,7 @@ impl Config {
     /// Get software license.
     ///
     /// # Returns
-    /// String representation of the software license.
+    /// - String representation of the software license.
     pub const fn license() -> &'static str {
         env!("CARGO_PKG_LICENSE")
     }
@@ -49,7 +49,7 @@ impl Config {
     /// Get software authors.
     ///
     /// # Returns
-    /// String representation of the software authors.
+    /// - String representation of the software authors.
     pub const fn authors() -> &'static str {
         env!("CARGO_PKG_AUTHORS")
     }
@@ -57,8 +57,46 @@ impl Config {
     /// Get software repository link.
     ///
     /// # Returns
-    /// String representation of the software repository link.
+    /// - String representation of the software repository link.
     pub const fn repository() -> &'static str {
         env!("CARGO_PKG_REPOSITORY")
+    }
+
+    /// Get software title.
+    ///
+    /// # Returns
+    /// - String representation of the software title.
+    pub const fn title() -> &'static str {
+        r#"
+ ██████╗██╗  ██╗██╗██████╗  █████╗
+██╔════╝██║  ██║██║██╔══██╗██╔══██╗
+██║     ███████║██║██████╔╝╚█████╔╝
+██║     ██╔══██║██║██╔═══╝ ██╔══██╗
+╚██████╗██║  ██║██║██║     ╚█████╔╝
+ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝      ╚════╝
+        "#
+    }
+
+    /// Get software list of available commands.
+    ///
+    /// # Returns
+    /// - List of available commands.
+    pub const fn help() -> &'static str {
+        r#"
+USAGE
+       chip8 [options] <file>
+
+
+DESCRIPTION
+
+       chip8 - CHIP-8 interpreted programming language emulator
+
+OPTIONS
+
+        -d,    --disasm     run in disassembler mode
+        -e,    --emulator   run in emulator mode
+        -h,    --help       display options list
+        -v,    --version    display version of hexd
+        "#
     }
 }
